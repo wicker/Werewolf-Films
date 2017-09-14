7d6d2ecf9086e68ea1698ef295e85572c1f7ee3e
Jenner Hanni <jenner@wickerbox.net>
2017-09-13 22:02:51 -0700

    Create list of movies and print to webpage.

--- movies.py
+++ movies.py
@@ -2,25 +2,52 @@ import os,webbrowser
 import urllib.request
 import json
 
+movies = []
+movies_json_file = 'movies.json'
+
 class Movie():
 
-  movies = []
+  def __init__(self,movie):
 
-  def __init__(self, movie):
-    self.title = movie.title
-    self.year = movie.year
-    self.plot = movie.plot
-    self.poster = movie.poster
+    self.title = movie['Title']
+    self.year = movie['Year']
+    self.plot = movie['Plot']
+    self.poster = movie['Poster']
     self.trailer = ''
 
-  def create_html_page():
+  def print_movie_info(self):
 
-    html_file = 'index.html'
+    print(self.title+' ('+self.year+')')
+    print(self.plot)
+    print('')
 
-    with open(html_file, 'w') as f:
-      f.write('Test')
+def populate_movies_list_from_json():
 
-    url = os.path.abspath(f.name)
-    webbrowser.open('file://'+url,new=2)
+  # open json, load first item
+  with open(movies_json_file,'r') as f:
+    movie_json = json.load(f)
 
-print("Test")
+  for entry in movie_json:
+    m = Movie(entry)
+    movies.append(m)
+
+  for m in movies:
+    m.print_movie_info()
+
+  return movies
+
+def create_html_page(movie_list):
+
+  html_file = 'index.html'
+
+  with open(html_file, 'w') as f:
+    for m in movie_list:
+      f.write(m.title+' ('+m.year+')<br />')
+      f.write(m.plot+'<br /><br />')
+
+  url = os.path.abspath(f.name)
+  webbrowser.open('file://'+url,new=2)
+
+movie_list = populate_movies_list_from_json()
+create_html_page(movie_list)
+
